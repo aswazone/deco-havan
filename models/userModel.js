36aaee77ef26635786177312c20adc79363f1fbd
aswazone <aswamanu143@gmail.com>
2024-11-15 15:03:23 +0530

    feat: set up User model with schema definition and basic validation

---
+++ models/userModel.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:false  
+    },
+    phone:{
+        type:String,
+        required:false,
+        unique:false,
+        spares:true,
+        default:null
+    },
+    googleId:{
+        type:String,
+        unique:true,
+    },
+    isBlocked:{
+        type:Boolean,
+        default:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    cart:[{
+        type:Schema.Types.ObjectId,
+        ref:'Cart'
+    }],
+    wallet:{
+        type:Number,
+        default:0
+    },
+    wishlist:[{
+        type:Schema.Types.ObjectId,
+        ref:'Wishlist'
+    }],
+    orderHistory:[{
+        type:Schema.Types.ObjectId,
+        ref:'Order'
+    }],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    referalCode:{
+        type:String,
+    },
+    redeemed:{
+        type:Boolean
+    },
+    redeemedUsers:[{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    }],
+    searchHistory:[{
+        category:{
+            type:Schema.Types.ObjectId,
+            ref:'Category'
+        },
+        brand:{
+            type:String
+        },
+        searchOn:{
+            type:Date,
+            default:Date.now
+        }
+    }]
+})
+
+const User = mongoose.model("User",userSchema)
+module.exports = User;
